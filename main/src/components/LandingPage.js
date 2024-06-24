fec6a97ded647260d54d60373102dbe6da6b961f
dsj47 <dsj47@cornell.edu>
2024-06-24 09:50:50 -0700

    fk tailwind, refactoring to j plain css

--- main/src/components/LandingPage.js
+++ main/src/components/LandingPage.js
@@ -5,103 +5,43 @@ import "./LandingPage.css"; // Import custom styles
 
 function LandingPage() {
   return (
-    <div class="wrapper">
-      <div class="box1">
-        <h1 className="font-bold uppercase mb-8">David</h1>
+    <div className="wrapper animate__animated animate__fadeIn">
+      <div className="name">
+        <div className="box1">
+          <div>DAVID</div>
+        </div>
+
+        <div className="box2">
+          <div>JEONG</div>
+        </div>
       </div>
 
-      <div class="box1">
-        <h1>Jeong</h1>
-      </div>
+      <div className="pages">
+        <div class="box3">
+          <div>
+            <Link to="/resume">Resume</Link>
+          </div>
+        </div>
 
-      <div class="box2">
-        <h2 className="text-2xl font-bold animate__animated animate__fadeInLeft">
-          <Link
-            to="/resume"
-            className="hover:text-blue-500 transition-colors duration-300"
-          >
-            Resume
-          </Link>
-        </h2>
-      </div>
+        <div className="box4">
+          <div>
+            <Link to="/projects">Projects</Link>
+          </div>
+        </div>
 
-      <div class="box3">
-        <h2 className="text-2xl font-bold animate__animated animate__fadeInRight">
-          <Link
-            to="/projects"
-            className="hover:text-blue-500 transition-colors duration-300"
-          >
-            Projects
-          </Link>
-        </h2>
-      </div>
+        <div className="box5">
+          <div>
+            <Link to="/profile">Profile</Link>
+          </div>
+        </div>
 
-      <div class="box4">
-        <h2 className="text-2xl font-bold animate__animated animate__fadeInDown">
-          <Link
-            to="/profile"
-            className="hover:text-blue-500 transition-colors duration-300"
-          >
-            Profile
-          </Link>
-        </h2>
-      </div>
-
-      <div class="box5">
-        <h2 className="text-2xl font-bold animate__animated animate__fadeInUp">
-          <Link
-            to="/blogs"
-            className="hover:text-blue-500 transition-colors duration-300"
-          >
-            Blogs!
-          </Link>
-        </h2>
+        <div className="box6">
+          <div>
+            <Link to="/blogs">Blogs</Link>
+          </div>
+        </div>
       </div>
     </div>
-
-    // <div className="bg-black text-white h-screen flex items-center justify-center">
-    //   <div className="container mx-auto">
-    //     <div className="flex flex-col items-center justify-center h-full">
-    //       <h1 className="text-6xl font-bold uppercase mb-8 animate__animated animate__fadeIn">
-    //         David Jeong
-    //       </h1>
-    //       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 text-center">
-    //         <h2 className="text-2xl font-bold animate__animated animate__fadeInLeft">
-    //           <Link
-    //             to="/resume"
-    //             className="hover:text-blue-500 transition-colors duration-300"
-    //           >
-    //             Resume
-    //           </Link>
-    //         </h2>
-    //         <h2 className="text-2xl font-bold animate__animated animate__fadeInRight">
-    //           <Link
-    //             to="/projects"
-    //             className="hover:text-blue-500 transition-colors duration-300"
-    //           >
-    //             Projects
-    //           </Link>
-    //         </h2>
-    //         <h2 className="text-2xl font-bold animate__animated animate__fadeInDown">
-    //           <Link
-    //             to="/profile"
-    //             className="hover:text-blue-500 transition-colors duration-300"
-    //           >
-    //             Profile
-    //           </Link>
-    //         </h2>
-    //         <h2 className="text-2xl font-bold animate__animated animate__fadeInUp">
-    //           <Link
-    //             to="/blogs"
-    //             className="hover:text-blue-500 transition-colors duration-300"
-    //           >
-    //             Blogs
-    //           </Link>
-    //         </h2>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
