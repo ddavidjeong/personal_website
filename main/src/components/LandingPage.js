8d5655914b08d6a30bb554521da2b1e39c727d6f
dsj47 <dsj47@cornell.edu>
2024-06-27 23:18:17 -0700

    centered a div

--- main/src/components/LandingPage.js
+++ main/src/components/LandingPage.js
@@ -5,47 +5,49 @@ import "./LandingPage.css"; // Import custom styles
 
 function LandingPage() {
   return (
-    <div className="wrapper animate__animated animate__fadeIn">
-      <div className="name">
-        <div className="box1">
-          <div>DAVID</div>
-        </div>
+    <div className="sitebox">
+      <div className="wrapper animate__animated animate__fadeIn">
+        <div className="name">
+          <div className="box1">
+            <div>DAVID</div>
+          </div>
 
-        <div className="box2">
-          <div>JEONG</div>
-        </div>
-      </div>
-
-      <div className="pages">
-        <div class="box3">
-          <div>
-            <Link to="/resume" className="links">
-              RESUME
-            </Link>
+          <div className="box2">
+            <div>JEONG</div>
           </div>
         </div>
 
-        <div className="box4">
-          <div>
-            <Link to="/projects" className="links">
-              PROJECTS
-            </Link>
+        <div className="pages">
+          <div class="box3">
+            <div>
+              <Link to="/resume" className="links">
+                RESUME
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className="box5">
-          <div>
-            <Link to="/profile" className="links">
-              PROFILE
-            </Link>
+          <div className="box4">
+            <div>
+              <Link to="/projects" className="links">
+                PROJECTS
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className="box6">
-          <div>
-            <Link to="/blogs" className="links">
-              BLOGS
-            </Link>
+          <div className="box5">
+            <div>
+              <Link to="/profile" className="links">
+                PROFILE
+              </Link>
+            </div>
+          </div>
+
+          <div className="box6">
+            <div>
+              <Link to="/blogs" className="links">
+                BLOGS
+              </Link>
+            </div>
           </div>
         </div>
       </div>
